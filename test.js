e58c571cfb05fe78b1a248a515ec29a488823390
Matteo Collina <hello@matteocollina.com>
2016-11-06 13:54:52 +0000

    Call the callback with `this` set to the stream instance

--- test.js
+++ test.js
@@ -10,36 +10,41 @@ var ws = fs.createWriteStream('/dev/null');
 eos(ws, function(err) {
 	expected--;
 	assert(!!err);
+	assert(this === ws);
 	if (!expected) process.exit(0);
 });
 ws.close();
 
-var rs = fs.createReadStream('/dev/random');
-eos(rs, function(err) {
+var rs1 = fs.createReadStream('/dev/random');
+eos(rs1, function(err) {
 	expected--;
 	assert(!!err);
+	assert(this === rs1);
 	if (!expected) process.exit(0);
 });
-rs.close();
+rs1.close();
 
-var rs = fs.createReadStream(__filename);
-eos(rs, function(err) {
+var rs2 = fs.createReadStream(__filename);
+eos(rs2, function(err) {
 	expected--;
 	assert(!err);
+	assert(this === rs2);
 	if (!expected) process.exit(0);
 });
-rs.pipe(fs.createWriteStream('/dev/null'));
+rs2.pipe(fs.createWriteStream('/dev/null'));
 
-var rs = fs.createReadStream(__filename);
-eos(rs, function(err) {
-	throw new Error('no go')
+var rs3 = fs.createReadStream(__filename);
+eos(rs3, function(err) {
+	assert(this === rs);
+	throw new Error('no go');
 })();
-rs.pipe(fs.createWriteStream('/dev/null'));
+rs3.pipe(fs.createWriteStream('/dev/null'));
 
 var exec = cp.exec('echo hello world');
 eos(exec, function(err) {
 	expected--;
 	assert(!err);
+	assert(this === exec);
 	if (!expected) process.exit(0);
 });
 
@@ -47,6 +52,7 @@ var spawn = cp.spawn('echo', ['hello world']);
 eos(spawn, function(err) {
 	expected--;
 	assert(!err);
+	assert(this === spawn);
 	if (!expected) process.exit(0);
 });
 
@@ -54,12 +60,14 @@ var socket = net.connect(50000);
 eos(socket, function(err) {
 	expected--;
 	assert(!!err);
+	assert(this === socket);
 	if (!expected) process.exit(0);
 });
 
 var server = net.createServer(function(socket) {
 	eos(socket, function() {
 		expected--;
+		assert(this === socket);
 		if (!expected) process.exit(0);
 	});
 	socket.destroy();
@@ -67,6 +75,7 @@ var server = net.createServer(function(socket) {
 	var socket = net.connect(30000);
 	eos(socket, function() {
 		expected--;
+		assert(this === socket);
 		if (!expected) process.exit(0);
 	});
 });
