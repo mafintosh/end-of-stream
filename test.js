52106b03c5cc9a9c6aaf60584a40cfda8daf8a5c
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-05-04 01:23:45 -0700

    Add failing pumpify test (#17)
    * Add failing pumpify test
    
    * Bump pumpify to show error still exists
    
    * Add success failure to show difference with failure

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 var assert = require('assert');
 var eos = require('./index');
 
-var expected = 8;
+var expected = 10;
 var fs = require('fs');
 var cp = require('child_process');
 var net = require('net');
+
+var pumpify = require('pumpify');
+var through = require('through2');
 
 var ws = fs.createWriteStream('/dev/null');
 eos(ws, function(err) {
@@ -78,6 +81,36 @@ var server = net.createServer(function(socket) {
 		assert(this === socket);
 		if (!expected) process.exit(0);
 	});
+});
+
+// Succeeds
+var rs4 =	fs.createReadStream('/dev/random');
+var p1 = pumpify(
+	through(),
+	through(function(chunk, _, cb) {
+		cb(new Error('test'));
+	}),
+	through()
+)
+eos(rs4.pipe(p1), function(err) {
+	expected--;
+	assert(err);
+	assert(err.message !== 'premature close', 'does not close with premature close');
+});
+
+// Fails
+var rs5 =	fs.createReadStream('/dev/random');
+var pumpifyErr = pumpify(
+	through(),
+	through(function(chunk, _, cb) {
+		cb(new Error('test'));
+	}),
+	fs.createWriteStream('/dev/null')
+)
+eos(rs5.pipe(pumpifyErr), function(err) {
+	expected--;
+	assert(err);
+	assert(err.message !== 'premature close', 'does not close with premature close');
 });
 
 setTimeout(function() {
