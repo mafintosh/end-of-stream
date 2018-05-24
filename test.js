3c3f2c2bc546e2fe23e6794e7a7fb71a590a2cc0
Thomas Watson <w@tson.dk>
2018-05-24 17:21:11 +0200

    Add test of http streams (#21)
    This broke in Node.js 10.2.0 because of:
    https://github.com/nodejs/node/pull/20611

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 var assert = require('assert');
 var eos = require('./index');
 
-var expected = 8;
+var expected = 10;
 var fs = require('fs');
 var cp = require('child_process');
 var net = require('net');
+var http = require('http');
 
 var ws = fs.createWriteStream('/dev/null');
 eos(ws, function(err) {
@@ -80,6 +81,24 @@ var server = net.createServer(function(socket) {
 		assert.ifError(err);
 		assert(this === socket);
 		if (!expected) process.exit(0);
+	});
+});
+
+var server2 = http.createServer(function(req, res) {
+	eos(res, function(err) {
+		expected--;
+		assert.ifError(err);
+	});
+	res.end();
+}).listen(function() {
+	var port = server2.address().port;
+	http.get('http://localhost:' + port, function(res) {
+		eos(res, function(err) {
+			expected--;
+			assert.ifError(err);
+			server2.close();
+		});
+		res.resume();
 	});
 });
 
